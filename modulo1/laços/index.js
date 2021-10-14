26dab61db08fe3b49790c60861bbee32990bf6dc
LarissaLanes <llarissalanep@gmail.com>
2021-10-14 19:53:16 -0300

    Exercícios da auala laços

---
+++ modulo1/laços/index.js
@@ -0,0 +1,23 @@
+//EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+//1 vai imprimir 5 o loop, pq enquanto 0 for menor que 5
+//2
+//A)só números maiores que 18 vão ser impressos no console
+//3
+//a programa irá rodar até chegar no número 3
+
+//EXERCÍCIOS DE ESCRITA DE CÓDIGOS
+//1
+let numeroDePets =Number(prompt("Quantos pets você tem?"))
+let total = 0
+
+if (numeroDePets > 0){
+   total = 0
+}while (total === numeroDePets){
+   const nomeDoPet = prompt("Diga o nome do seu pet:")
+   let nomes = []
+   nomes.push(nomeDoPet)
+   total =+ numeroDePets
+}else {
+console.log("Que pena!,adote um Pet!")
+}
+console.log (nomes)
