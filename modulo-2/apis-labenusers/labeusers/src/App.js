8a559688c2b2b4cf398c461b38c6c1e233d87fb2
LarissaLanes <llarissalanep@gmail.com>
2021-11-18 12:13:32 -0300

    Aula-Apis

--- modulo-2/apis-labenusers/labeusers/src/App.js
+++ modulo-2/apis-labenusers/labeusers/src/App.js
@@ -1,65 +1,44 @@
-import axios from 'axios';
 import React from 'react';
+import PageRegister from './components/PageRegister';
+import PageList from './components/PageList';
 // import Styled from 'styled-components'
 
 export default class App extends React.Component{
-
-
   state = {
-    dataUsers: [],
-    inputValueName: "",
-    inputValueEmail:"",
-
-  }
-
-  componentDidMount(){
-    this.createUser();
-  }
-
-  onChangeEnviarDados = (event) => {
-    this.setState({ inputValueName: event.target.value });
-  };
-
-  createUser = () => {
-    const body = {
-      'name': this.state.inputValueName,
-      'email':this.state.inputValueEmail,
-    };
-    axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", body,
-    {
-      headers:{
-        Authorization: "larissa-lanes-carver"
-      }
-    })
-    .then((res) => {
-      this.setState({ inputValueName: '' });
-      this.setState({ inputValueEmail: "" });
-      })
-    .catch((err) => {
-      console.log('deu errado!')
-    })
-  };
+    page: "cadastro",
     
-  
+  }
+
+  /////Mudar de tela///////////////////////////////
+
+  selectPage = () => {
+    switch(this.state.page){
+      case "cadastro":
+       return <PageRegister goToList={this.goToList}/>
+      case "lista":
+        return <PageList goToRegister={this.goToRegister}/>
+      default:
+       return <div>erro!</div>
+    }
+  }
+
+  goToRegister = () => {
+    this.setState({page: "cadastro"})
+  };
+
+  goToList = () => {
+    this.setState({page: "lista"})
+  };
+
+  /////Fim de mudar a tela/////////////////////
+
+ 
+
   render(){
     
     return(
       <div >
-        <div>
-        <p>header</p>
-        </div>
-        <div>
-          <h3>TELA DE CADASTRO</h3>
-          <input placeholder={"nome"}/>
-          <input placeholder={"email"}/>
-          <button onClick={this.createUser} value={this.state.inputValueName} onChange={this.onChangeEnviarDados}>CADASTRAR DADOS</button>
-        
-          onChange={this.handleInputChange}
-        </div>
-
-
-       
-
+        {this.selectPage()}
       </div>
     )
   }
