16c4653f39f5f7ce1479f75bbb39f188f04c1520
LarissaLanes <llarissalanep@gmail.com>
2022-01-19 20:33:41 -0300

    Desáfios e exercícios finais

--- modulo-4/intro-typescript/src/exercicio1.ts
+++ modulo-4/intro-typescript/src/exercicio1.ts
@@ -1,3 +1,13 @@
-const idade: Number = Number(process.argv[2])
+//EXERCÍCIO 1
 
-console.log(idade)
+function checaTriangulo(a: Number, b: Number, c: Number): string{
+    if (a !== b && b !== c) {
+      return "Escaleno";
+    } else if (a === b && b === c) {
+      return "Equilátero";
+    } else {
+      return "Isósceles";
+    }
+}
+
+console.log(checaTriangulo(4, 5, 2))
