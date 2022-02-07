ad99826eb31df60b3636b02357e1aed63fc67c22
LarissaLanes <llarissalanep@gmail.com>
2022-02-07 20:06:25 -0300

    Async-await-promises

---
+++ modulo-4/async-await-promises/src/URL_BASE.ts
@@ -0,0 +1,1 @@
+export const URL_BASE = "https://labenews.herokuapp.com"
---
+++ modulo-4/async-await-promises/src/ex1.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import { URL_BASE } from './URL_BASE';
+
+const main = async () => {
+    console.log('tá rolando uma função async!')
+}
+
+main()
