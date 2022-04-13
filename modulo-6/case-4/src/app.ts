aee02268c31e24a9b45f33931c91b1f88cda1d61
LarissaLanes <llarissalanep@gmail.com>
2022-04-13 13:56:57 -0300

    deploy e readme.md

--- modulo-6/case-4/src/app.ts
+++ modulo-6/case-4/src/app.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
 import express from "express";
-import { AddressInfo } from "net";
+
 import dotenv from "dotenv"
 
 dotenv.config();
@@ -10,13 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const server = app.listen(3005, () => {
-    if(server){
-        const address = server.address() as AddressInfo;
-        console.log(`Servidor rodando em http://localhost:${address.port}`)
-    }else{
-        console.error(`falha ao rodar o servidor`)
-    }
+app.listen(process.env.PORT || 3005, () => {
+    console.log(`Servidor rodando em http://localhost:3005`)
 })
+
 
 export default app;
